9598678e7f5053eb2ad02a9a6dd6df4cb70f6b34
ucsd24student <163366683+ucsd24student@users.noreply.github.com>
2024-09-10 10:18:13 -0700

    Add files via upload

---
+++ mpt.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+from sklearn.impute import KNNImputer
+from scipy.optimize import minimize
+# import streamlit_setup as ss
+
+history = pd.read_csv('/Users/sarthakdoshi/Documents/Website/dashboard/Stocks Quarterly Price Percent Change Table Since 2014.csv')
+def covariance_fc(history,df):
+    mat = history[history['Ticker'].isin(df['Ticker'])]
+    mat = mat.T
+    mat.columns = mat.iloc[0,:]
+    mat = mat.iloc[1:,:]
+    imputer = KNNImputer(n_neighbors=10)
+    mat_filled = imputer.fit_transform(mat)
+    pctchg = mat_filled/100
+    pctchg_vol = pctchg.std()
+    arith_mean = np.mean(pctchg,axis=0)
+    pctchg_centered = pctchg - arith_mean
+    V = pctchg_centered.T.dot(pctchg_centered)/pctchg.shape[0]  # historic covariance matrix
+    gamma = np.asarray(df['IV'])
+    factor = gamma/pctchg_vol # scaling factor for diagonal scaling method
+    D = np.diag(factor)  # diagonal scaling matrix
+    forecasted_cov = D @ V @ D # forecasted covariance
+    return forecasted_cov
+
+# Portfolio statistics functions
+def portfolio_volatility(weights, cov_matrix):
+    return np.dot(weights.T, np.dot(cov_matrix, weights))
+
+# Define the constraint: portfolio return should be equal to the target return
+def portfolio_return(weights, expected_returns):
+    return np.dot(weights, expected_returns)
+
+# Define the function to minimize the negative Sharpe ratio
+def negative_sharpe_ratio(weights, expected_returns, cov_matrix, risk_free_rate):
+    port_return = portfolio_return(weights, expected_returns)
+    port_variance = portfolio_volatility(weights, cov_matrix)
+    return -(port_return - risk_free_rate) / np.sqrt(port_variance)
+
+# Define the constraint function for the target return
+def return_constraint(weights, expected_returns, target_return):
+    return portfolio_return(weights, expected_returns) - target_return
+
+def mpt(mat,expected_returns):
+    n_assets = mat.shape[0]
+    bounds = [(0, 0.1) for _ in range(n_assets)]
+    risk_free_rate = 0.06
+    # portfolio_returns = np.arange(0.08,0.25,0.02)
+    opt_weights,opt_returns,opt_volatility,opt_sharpe_ratio = [],[],[],[]
+    # Optimize portfolio
+    expected_returns = expected_returns/100
+    for ret in expected_returns:
+        # Portfolio weights
+        initial_weights = np.ones(n_assets) / n_assets
+        # initial_weights = initial_weights / np.sum(initial_weights)
+        constraints = ({
+                            'type': 'eq',
+                            'fun': lambda weights: np.sum(weights) - 1
+                        }, {
+                            'type': 'eq',
+                            'fun': lambda weights: return_constraint(weights, expected_returns, ret)
+                        })
+        result = minimize(
+            fun=negative_sharpe_ratio,
+            x0=initial_weights,
+            args=(expected_returns, mat, risk_free_rate),
+            method='SLSQP',
+            bounds=bounds,
+            constraints=constraints
+        )
+
+        # Optimal weights
+        weights = result.x
+        returns = portfolio_return(weights, expected_returns)
+        volatility = portfolio_volatility(weights, mat)
+        sharpe_ratio = -result.fun
+
+        opt_weights.append(weights)
+        opt_returns.append(returns)
+        opt_volatility.append(volatility)
+        opt_sharpe_ratio.append(sharpe_ratio)
+        
+    return opt_returns,opt_volatility,opt_weights
+
+def generate_portfolio(df):
+    ticks = list(set(df.Ticker) & set(history.Ticker))
+    df = df[df['Ticker'].isin(ticks)]
+    mat = covariance_fc(history,df)
+    returns,volatility,weights = mpt(mat,df['Total Return'])
+    return(returns,volatility,weights)
+
---
+++ streamlit_setup.py
@@ -0,0 +1,97 @@
+import streamlit as st
+import pandas as pd
+import mpt
+import yfinance as yf
+
+# Load the CSV data
+csv_file = '/Users/sarthakdoshi/Documents/Valuation/BCAstocks_8_27.csv'
+indices = pd.read_csv('/Users/sarthakdoshi/Documents/Website/dashboard/indices.csv')
+df = pd.read_csv(csv_file)
+# Display the full table
+# st.write("Full Table")
+# st.dataframe(df)
+
+st.sidebar.header('Filter Options')
+
+iv_values = st.sidebar.slider('Implied Volatility', 0.0, df['IV'].max(),value=[0.0,40.0])
+tot_return_values = st.sidebar.slider('Forecasted Total Returns in %', 0.0, 200.0,value=[8.0,100.0])
+# mar_cap_values = st.sidebar.slider('Market Cap in $Bn', 0.004, 3287.0,value=[0.01, 10.0])
+etfs = ['All']
+etfs.extend(df['Sector'].unique())
+pe_values = st.sidebar.slider('Current PE Ratio', df['PE Ratio'].min(),df['PE Ratio'].max(),value=[6.0,80.0])
+div_values = st.sidebar.slider('Dividend Yield in %', df['DividendYield'].min(),df['DividendYield'].max(),value=[0.01,5.0])
+min_market = st.sidebar.number_input("Min Market Cap in $Mn ", min_value=0, max_value=3287000, value=10)
+max_market = st.sidebar.number_input("Max Market Cap in $Mn", min_value=0, max_value=3287000, value=1000)
+etf_selections = st.sidebar.multiselect('Select ETF',etfs,default='All')
+idx = ['All','S&P 500', 'DJIA', 'Nasdaq 100']
+idx_selection = st.sidebar.multiselect('Index',idx,default=['All'])
+
+# Filter the dataframe based on user input
+if iv_values:
+    df = df[df['IV'].between(iv_values[0],iv_values[1])]
+
+if tot_return_values:
+    df = df[df['Total Return'].between(tot_return_values[0],tot_return_values[1])]
+
+if min_market or max_market:
+    df = df[df['Market Cap. (USD)'].between(min_market*1000000,max_market*1000000)]
+
+if pe_values:
+    df = df[df['PE Ratio'].between(pe_values[0],pe_values[1])]
+
+if div_values:
+    df = df[df['DividendYield'].between(div_values[0],div_values[1])]
+
+if etf_selections and not 'All' in etf_selections:
+    df = df[df['Sector'].isin(etf_selections)]
+
+if idx_selection and not 'All' in idx_selection:
+    tckr = set()
+    for ind in idx_selection:
+        tckr = tckr or set(indices[ind].dropna())
+    df = df[df['Ticker'].isin(list(tckr))]
+
+# Display headers
+spy, dji, intrate = st.columns(3)
+
+with spy:
+    sp500 = yf.Ticker("SPY")
+    chg = float(round((sp500.history()['Close'].iloc[-1] - sp500.history()['Close'].iloc[-2])/sp500.history()['Close'].iloc[-2]*100,2))
+    st.metric(label='SPY', value=sp500.info['regularMarketOpen'], delta=f"{chg}%")
+
+with dji:
+    djindex = yf.Ticker("^DJI")
+    chg = float(round((djindex.history()['Close'].iloc[-1] - djindex.history()['Close'].iloc[-2])/djindex.history()['Close'].iloc[-2]*100,2))
+    st.metric(label='DJI', value=djindex.info['regularMarketOpen'], delta=f"{chg}%")
+
+with intrate:
+    irate = yf.Ticker("^TYX")
+    chg = float(round((irate.history()['Close'].iloc[-1] - irate.history()['Close'].iloc[-2])/irate.history()['Close'].iloc[-2]*100,2))
+    st.metric(label='10Y Yield Rate', value=round(irate.info['regularMarketOpen'],2), delta=f"{chg}%")
+
+# Display the filtered table
+st.write("Filtered Table")
+st.dataframe(df)
+
+st.write("IV - Total Return")
+chart_data = df[['IV', 'Total Return']]
+st.scatter_chart(chart_data,x='IV',y='Total Return')
+
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+
+def click_button():
+    st.session_state.clicked = True
+
+st.sidebar.button('Generate portfolio', on_click=click_button)
+
+if st.session_state.clicked:
+    ret, vol, wgts = mpt.generate_portfolio(df)
+    frontier = pd.DataFrame([ret,vol])
+    frontier = frontier.T
+    frontier.columns = ['Expected Returns','Volatility']
+    st.write("Efficient Frontier")
+    st.scatter_chart(frontier,x='Volatility',y='Expected Returns')
+    # st.write('Allocations')
+    # st.bar_chart(wgts,x='Ticker',y='Weights')
+    # st.sidebar.write('Creating Portfolio')
